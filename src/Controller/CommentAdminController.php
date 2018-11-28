70b2e34211c1793c3aa5d14ac357cd3b9b6df8b5
Romain Helou <helouromain@gmail.com>
2018-11-28 14:20:44 +0100

    21 - Role Hierarchy

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 /**
  * Class CommentAdminController
  * @package App\Controller
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_ADMIN_COMMENT")
  */
 class CommentAdminController extends Controller
 {
